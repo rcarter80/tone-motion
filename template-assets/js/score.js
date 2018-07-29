f1df03ceed126eb0e9c6073f377f1b7f9d23f903
Ryan Carter <ryan@ryancarter.org>
2018-07-29 17:34:58 -0400

    So far so good

--- template-assets/js/score.js
+++ template-assets/js/score.js
@@ -1,45 +1,6 @@
+const tm = new ToneMotion();
+tm.debug = true;
 
-// Test cues
-cueList[1] = new TMCue(2000, 0);
-cueList[1].mode = 'tilt';
-cueList[1].goCue = function() {
-  console.log('cueList[1].goCue() called');
-}
-
-cueList[2] = new TMCue(0, 0);
-cueList[2].goCue = function() {
-  console.log('cueList[2].goCue() called');
-}
-
-cueList[3] = new TMCue(500, 0);
-cueList[3].goCue = function() {
-  console.log('cueList[3].goCue() called');
-}
-
-cueList[4] = new TMCue(3000, 0);
-cueList[4].goCue = function() {
-  console.log('cueList[4].goCue() called at ' + Date.now());
-}
-cueList[4].stopCue = function() {
-  console.log('cueList[4].stopCue() called at ' + Date.now());
-}
-
-cueList[5] = new TMCue(20000, 0);
-cueList[5].goCue = function() {
-  console.log('cueList[5].goCue() called at ' + Date.now());
-}
-
-cueList[7] = new TMCue(1000, 500);
-cueList[7].goCue = function() {
-  console.log('cueList[7].goCue() called');
-}
-
-cueList[8] = new TMCue(500, 0);
-cueList[8].goCue = function() {
-  console.log('cueList[8].goCue() called');
-}
-
-cueList[9] = new TMCue(-1);
-cueList[9].goCue = function() {
-  console.log('cueList[9].goCue() called AS SOON AS I CAN at ' + Date.now());
-}
+window.onload = function() {
+  tm.init();
+};
--- tonemotion-shared/js/tonemotion-1.1.0.js
+++ tonemotion-shared/js/tonemotion-1.1.0.js
@@ -2,14 +2,123 @@
 ************************ APPLICATION SETUP ***************************
 *********************************************************************/
 
+/*
+** DOM HOOKS
+*/
+
+const statusLabel = document.querySelector('#statusLabel');
+const startStopButton = document.querySelector('#startStopButton');
+const messageLabel = document.querySelector('#messageLabel');
+const consoleCheckbox = document.querySelector('#consoleCheckbox');
+const publicConsole = document.querySelector('#publicConsole');
+
 /**
- * Global object to encapsulate properties and method for Tone Motion
+ * Object to encapsulate properties and methods for Tone Motion
  * @param {string} status - Application status (set automatically)
+ * @param {boolean} debug - Can set to 'true' in score.js
+ * @param {number} clientServerOffset - (ms.) Adjustment to client time
  */
 
 function ToneMotion() {
   this.status = 'loading';
+  this.debug = 'false';
+  this.clientServerOffset = 0;
 }
+
+ToneMotion.prototype.init = function() {
+  // Load test audio file into Tone.Buffer (same audio file as <audio> shim to tell Safari that page should play audio)
+  const bufferLoadingTestFile = new Tone.Buffer('tonemotion-shared/audio/silent-buffer-to-set-audio-session.mp3');
+
+  Tone.Buffer.on('progress', () => {
+    this.status = 'loading';
+    if (this.debug) {
+      this.publicLog('Audio buffers loading');
+    }
+  });
+
+  // Called when all buffers are done loading
+  Tone.Buffer.on('load', () => {
+    if (this.debug) {
+      this.publicLog('Audio buffers finished loading');
+    }
+    // Synchronize client clock to server once all resources loaded
+    this.syncClocks();
+  });
+
+  Tone.Buffer.on('error', () => {
+    console.log('error');
+  });
+
+  // TODO: begin motion handling
+}
+
+// Prints to console.log and to interface if consoleCheckbox is checked
+ToneMotion.prototype.publicLog = function(message) {
+  if (consoleCheckbox.checked) {
+    var logMessage = document.createElement('p');
+    logMessage.className = 'logMessage';
+    logMessage.innerHTML = message;
+    publicConsole.appendChild(logMessage);
+  }
+  console.log(message);
+}
+
+/*********************************************************************
+******************* CLIENT/SERVER SYNCHRONIZATION ********************
+*********************************************************************/
+
+// Synchronizes client time to server time
+const urlForClockSync = 'https://jack-cue-manager-test.herokuapp.com/test-server/clock-sync';
+
+ToneMotion.prototype.syncClocks = function() {
+  this.status = 'synchronizing';
+  var syncClockCounter = 0;
+  var shortestRoundtrip = Number.POSITIVE_INFINITY;
+
+  var syncClockID = setInterval( () => {
+    var syncTime1 = Date.now(); // client-side timestamp
+
+    fetch(urlForClockSync)
+    .then(response => response.text())
+    .then(response => {
+      var syncTime2 = response; // server-side timestamp
+      var syncTime3 = Date.now(); // client-side timestamp on receipt
+      var roundtrip = syncTime3 - syncTime1;
+      if (this.debug) {
+        this.publicLog('Time request number ' + syncClockCounter + ' sent at ' + syncTime1 + ' (client time). Response sent at ' + syncTime2 + ' (server time). Response received at ' + syncTime3 + ' (client time). Roundtrip latency: ' + roundtrip + ' milliseconds.');
+      }
+      if (roundtrip < shortestRoundtrip) {
+        // safari caches response despite my very nice request not to
+        // it releases cache after first iteration, but if first try
+        // is super short roundtrip (e.g., 1 ms), the result is b.s.
+        if (syncClockCounter > 1 || roundtrip > 10) {
+          shortestRoundtrip = roundtrip;
+          // shortest roundtrip considered most accurate
+          // subtract TM.clientServerOffset from client time to sync
+          this.clientServerOffset = (syncTime3-syncTime2) - (roundtrip/2);
+        } else {
+          this.publicLog('It looks like the last response was served from the disk cache and is invalid.');
+        }
+      }
+      if (syncClockCounter === 6) { // last check
+        if (shortestRoundtrip > 2000) {
+          ;
+          // publicWarning('There seems to be a lot of latency in your connection to the server (' + shortestRoundtrip + ' milliseconds of round-trip delay). Your device may not be synchronized.');
+        } else {
+          this.publicLog('Shortest roundtrip latency was ' + shortestRoundtrip + ' milliseconds. Client time is estimated to be ahead of server time by ' + this.clientServerOffset + ' milliseconds.');
+        }
+        this.status = 'readyToPlay';
+      }
+    })
+    // .catch(error => publicError(error));
+
+    // stop after 6 checks (5 seconds)
+    if (++syncClockCounter === 6) {
+      window.clearInterval(syncClockID);
+    }
+  }, 1000);
+}
+
 
 /*********************************************************************
 ************************ CUE LIST MANAGEMENT *************************
