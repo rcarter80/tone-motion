d3f73a281232c786324d329bd6f17c2ec764e6f4
Ryan Carter <ryan@ryancarter.org>
2018-07-31 02:02:15 -0400

    same

--- tonemotion-shared/js/tonemotion-1.1.0.js
+++ tonemotion-shared/js/tonemotion-1.1.0.js
@@ -319,7 +319,7 @@ ToneMotion.prototype.bindButtonFunctions = function() {
 // Tests if device is Android, registers 'devicemotion' event listener
 ToneMotion.prototype.beginMotionHandling = function() {
   if (this.debug) {
-    this.publicLog('Beginning motion detection')
+    this.publicLog('Beginning motion detection.')
   }
 
   // Android devices report motion in same range as iOS but with inverted axes. Check if device is Android
