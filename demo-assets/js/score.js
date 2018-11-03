b9ae16c4e9e489c09533b474fb92ce558b414c03
Ryan Carter <ryan@ryancarter.org>
2018-11-03 19:55:12 -0400

    add TODO comments

--- demo-assets/js/score.js
+++ demo-assets/js/score.js
@@ -1,12 +1,14 @@
 const tm = new ToneMotion();
 tm.debug = true;
 tm.showConsoleOnLaunch = true;
-tm.shouldSyncToServer = false; // to speed up load time while testing
+tm.shouldSyncToServer = true; // to speed up load time while testing
 
 window.onload = function() {
   tm.init();
-  // tm.testWithoutMotion();
 };
+
+// Instruments need global scope within this file
+var synth = new Tone.Synth().toMaster();
 
 // Cue number 0 sets status to 'waitingForPieceToStart'
 tm.cue[0] = new TMCue('waiting', -1);
@@ -19,7 +21,6 @@ tm.cue[1] = new TMCue('tilt', 2000, 0);
 tm.cue[1].goCue = function() {
   tm.publicLog('tm.cue[1].goCue() called.');
 
-  var synth = new Tone.Synth().toMaster();
   synth.triggerAttackRelease("C4", 4);
 }
 tm.cue[1].updateTiltSounds = function() {
@@ -37,31 +38,10 @@ tm.cue[3].goCue = function() {
 }
 tm.cue[3].triggerShakeSound = function() {
   tm.publicLog('Shake gesture triggered at ' + Date.now());
-  synth.triggerAttackRelease("C4", 4);
+  synth.triggerAttackRelease("C4", 0.25);
 }
 
-tm.cue[4] = new TMCue('waiting', -1);
+tm.cue[4] = new TMCue('finished', -1);
 tm.cue[4].goCue = function() {
-  tm.publicLog('tm.cue[4].goCue() called at ' + Date.now());
-}
-tm.cue[4].stopCue = function() {
-  tm.publicLog('tm.cue[4].stopCue() called at ' + Date.now());
-}
-
-tm.cue[5] = new TMCue('tiltAndShake', 2000, 0);
-tm.cue[5].goCue = function() {
-  tm.publicLog('tm.cue[5].goCue() called at ' + Date.now());
-}
-
-tm.cue[7] = new TMCue('listen', 1000, 500);
-tm.cue[7].goCue = function() {
-  tm.publicLog('tm.cue[7].goCue() called');
-}
-
-tm.cue[8] = new TMCue('listen', 0, 0);
-// this cue is impossible to make on time and will throw missed cue
-
-tm.cue[9] = new TMCue('finished', -1);
-tm.cue[9].goCue = function() {
   tm.publicLog('The piece is done.');
 }
