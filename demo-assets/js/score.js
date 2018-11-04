a6244cd09acb8ef4c3e97cd0e682e6dc21d4d344
Ryan Carter <ryan@ryancarter.org>
2018-11-04 00:04:36 -0400

    Adding chimes for shake section

--- demo-assets/js/score.js
+++ demo-assets/js/score.js
@@ -11,6 +11,12 @@ window.onload = function() {
 // Instruments need global scope within this file
 var synth = new Tone.Synth().toMaster();
 
+var chimePlayer = new Tone.Players({
+  "ch1714": "demo-assets/audio/chime-1714Hz-A6.mp3",
+  "ch1748": "demo-assets/audio/chime-1748Hz-A6.mp3"
+}).toMaster();
+var chimeArray = ["ch1714", "ch1748"];
+
 // Cue number 0 sets status to 'waitingForPieceToStart'
 tm.cue[0] = new TMCue('waiting', -1);
 tm.cue[0].goCue = function() {
@@ -35,11 +41,18 @@ tm.cue[2].goCue = function() {
 
 tm.cue[3] = new TMCue('shake', -1);
 tm.cue[3].goCue = function() {
+  // trigger random chime at beginning of SHAKE section
+  var thisChime = chimeArray[Math.floor(Math.random()*chimeArray.length)];
+  chimePlayer.get(thisChime).start();
+
   tm.publicLog('tm.cue[3].goCue() called');
 }
 tm.cue[3].triggerShakeSound = function() {
+  // trigger random chime in response to shake gesture
+  var thisChime = chimeArray[Math.floor(Math.random()*chimeArray.length)];
+  chimePlayer.get(thisChime).start();
+
   tm.publicLog('Shake gesture triggered at ' + Date.now());
-  synth.triggerAttackRelease("C4", 0.25);
 }
 
 tm.cue[4] = new TMCue('finished', -1);
