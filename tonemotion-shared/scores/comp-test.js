ff4b069b5c5e614ca535375c3437d2e3f113c13c
Ryan Carter <40380375+rcarter80@users.noreply.github.com>
2021-06-02 18:03:56 -0400

    migrating to new Tone.js which changed how loaded buffers are reported

---
+++ tonemotion-shared/scores/comp-test.js
@@ -0,0 +1,17 @@
+const tm = new ToneMotion();
+tm.debug = true; // if true, skips clock sync and shows console
+tm.localTest = false; // if true, fetches cues from localhost, not Heroku
+window.onload = function() {
+  // must initialize with URL for cue server, which is unique to piece
+  // fetch cues from localhost if tm.localTest is true
+  if (tm.localTest) {
+    tm.init('http://localhost:3000/jack-server/current-cue');
+  } else {
+    tm.init('https://tonemotion-cue-manager.herokuapp.com/jack-server/current-cue');
+  }
+};
+
+const player = new Tone.Player({
+  url: "tonemotion-shared/audio/misc/test-square.mp3",
+  loop: true,
+});
