296bb66a46200a29761f7421779fb3786b769fc4
Ryan Carter <40380375+rcarter80@users.noreply.github.com>
2021-06-09 13:18:40 -0400

    working on improving tutorials

--- tonemotion-shared/scores/comp-test.js
+++ tonemotion-shared/scores/comp-test.js
@@ -5,22 +5,80 @@ window.onload = function() {
   // must initialize with URL for cue server, which is unique to piece
   // fetch cues from localhost if tm.localTest is true
   if (tm.localTest) {
-    tm.init('http://localhost:3000/jack-server/current-cue');
+    tm.init('http://localhost:3000/shared-server/current-cue');
   } else {
-    tm.init('https://tonemotion-cue-manager.herokuapp.com/jack-server/current-cue');
+    tm.init('https://tonemotion-cue-manager.herokuapp.com/shared-server/current-cue');
   }
 };
 
-const player = new Tone.Player({
-  url: "tonemotion-shared/audio/misc/test-square.mp3",
-  loop: true,
-}).toDestination();
+// *******************************************************************
+// CUE 0: sets status to 'waitingForPieceToStart'
+tm.cue[0] = new TMCue('waiting', 0, NO_LIMIT);
+tm.cue[0].goCue = function() {
+  tm.publicLog('Waiting for piece to start');
+};
 
 // *******************************************************************
-tm.cue[0] = new TMCue('shake', -1);
-tm.cue[0].goCue = function() {
-  player.start();
+// CUE 1: tilt tutorial
+// Test tone for "tilt" tutorial
+var testToneFilter = new Tone.Filter(440, "lowpass").toDestination();
+var testTone = new Tone.Synth({
+  oscillator: {
+    type: "sawtooth"
+  },
+  envelope: {
+    attack: 0.005,
+    decay: 0.1,
+    sustain: 0.9,
+    release: 0.1
+  }
+}).connect(testToneFilter);
+testTone.volume.value = -12; // The music is not very loud, so let's encourage people to turn up volume.
+var testToneFreqScale = new Tone.Scale(440, 880); // scales control signal (0.0 - 1.0)
+var testToneFilterScale = new Tone.Scale(440, 10000);
+xTilt.chain(testToneFreqScale, testTone.frequency); // ctl sig is mapped to freq
+yTilt.chain(testToneFilterScale, testToneFilter.frequency);
+tm.cue[1] = new TMCue('tilt', 0, NO_LIMIT);
+tm.cue[1].goCue = function() {
+  testTone.triggerAttack(440);
+}
+tm.cue[1].updateTiltSounds = function() {
+  // interactivity handled through tm.xTilt and yTilt signals
+}
+tm.cue[1].stopCue = function() {
+  testTone.triggerRelease();
+}
+
+// *******************************************************************
+// CUE 2: tacet tutorial
+tm.cue[2] = new TMCue('tacet', 0, NO_LIMIT);
+tm.cue[2].goCue = function() {
+  // nothing to play
+}
+tm.cue[2].stopCue = function() {
+  // nothing to clean up
+}
+
+// *******************************************************************
+// CUE 3: shake tutorial
+var cowbell = new Tone.Player(perc_sounds + 'cowbell.mp3').toDestination();
+tm.cue[3] = new TMCue('shake', 0, NO_LIMIT);
+tm.cue[3].goCue = function() {
+  // nothing to do until shake gestures
 };
-tm.cue[0].stopCue = function() {
-  player.stop();
+tm.cue[3].triggerShakeSound = function() {
+  cowbell.start();
+};
+tm.cue[3].stopCue = function() {
+  // nothing to clean up
+};
+
+// *******************************************************************
+// CUE 4: sets status to 'waitingForPieceToStart'
+tm.cue[4] = new TMCue('waiting', 0, NO_LIMIT);
+tm.cue[4].goCue = function() {
+  tm.publicLog('Waiting for piece to start');
+};
+tm.cue[4].stopCue = function() {
+  // nothing to clean up
 };
