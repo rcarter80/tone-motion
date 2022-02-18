33f90ef60c2c980b8b493f4058bbf28ccd0e8858
Ryan Carter <40380375+rcarter80@users.noreply.github.com>
2022-02-17 23:44:15 -0500

    finished test site to distribute

--- tonemotion-shared/scores/test.js
+++ tonemotion-shared/scores/test.js
@@ -10,16 +10,36 @@ window.onload = function() {
   }
 };
 
-// Shortcuts to audio file path
-const demo_sounds = 'tonemotion-shared/audio/demo/';
+let meow = new Tone.Player('tonemotion-shared/audio/demo/meow.mp3').toDestination();
+
+function testMotion() {
+  tm.publicMessage('Testing: 10');
+  setTimeout(() => { tm.publicMessage('Testing: 9'); }, 1000);
+  setTimeout(() => { tm.publicMessage('Testing: 8'); }, 2000);
+  setTimeout(() => { tm.publicMessage('Testing: 7'); }, 3000);
+  setTimeout(() => { tm.publicMessage('Testing: 6'); }, 4000);
+  setTimeout(() => { tm.publicMessage('Testing: 5'); }, 5000);
+  setTimeout(() => { tm.publicMessage('Testing: 4'); }, 6000);
+  setTimeout(() => { tm.publicMessage('Testing: 3'); }, 7000);
+  setTimeout(() => { tm.publicMessage('Testing: 2'); }, 8000);
+  setTimeout(() => { tm.publicMessage('Testing: 1'); }, 9000);
+
+  setTimeout(() => {
+    if (tm.motionFailCount === 0) {
+      tm.publicMessage('Everything seems to be working fine! Thanks for checking!');
+    } else {
+      tm.publicMessage('Hmm, there seems to be a problem. Could you please take a screenshot of the following and email it to me? My email is ryan@ryancarter.org. Thanks so much! <br> user agent: ' + window.navigator.userAgent + '<br> motionFailCount: ' + tm.motionFailCount + '<br> accel x: ' + tm.accel.x + '<br> accel y: ' + tm.accel.y);
+    }
+  }, 10000);
+
+}
 
 // *******************************************************************
 // CUE 0: (same cue copied 3 times so that I can reuse network test server)
-var meow = new Tone.Player(demo_sounds + 'meow.mp3').toDestination();
 
 tm.cue[0] = new TMCue('shake', -1);
 tm.cue[0].goCue = function() {
-  // TODO: add "testing" countdown. Test against motionFailCount (if it's 0, everything is fine, and say so). if error, includes instructions: screenshot and send to me - include email address.
+  testMotion();
 };
 tm.cue[0].triggerShakeSound = function() {
   meow.start();
@@ -28,4 +48,24 @@ tm.cue[0].stopCue = function() {
   // nothing to clean up
 };
 
-// TODO: copy cue 0 two more times, bc test server has cue 2 as max
+tm.cue[1] = new TMCue('shake', -1);
+tm.cue[1].goCue = function() {
+  testMotion();
+};
+tm.cue[1].triggerShakeSound = function() {
+  meow.start();
+};
+tm.cue[1].stopCue = function() {
+  // nothing to clean up
+};
+
+tm.cue[2] = new TMCue('shake', -1);
+tm.cue[2].goCue = function() {
+  testMotion();
+};
+tm.cue[2].triggerShakeSound = function() {
+  meow.start();
+};
+tm.cue[2].stopCue = function() {
+  // nothing to clean up
+};
