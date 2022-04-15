9df52ccce4d2d3b140fde85d121c4d23d62f38ef
Ryan Carter <40380375+rcarter80@users.noreply.github.com>
2022-04-14 22:24:00 -0400

    bug fix in older version of tonemtion (using for load testing server)

--- tonemotion-shared/js/tonemotion-1.4.2.js
+++ tonemotion-shared/js/tonemotion-1.4.2.js
@@ -124,7 +124,7 @@ ToneMotion.prototype.init = function(urlOfServer) {
     this.shouldSyncToServer = false;
   }
   // Can automatically show console in left panel when page loads
-  // This was previously NOT the default behavior, but a scrolling issue in iOS Safari is fixed by displaying console by default. If debug mode is on, lots of messages are generated, but otherwise few are. 
+  // This was previously NOT the default behavior, but a scrolling issue in iOS Safari is fixed by displaying console by default. If debug mode is on, lots of messages are generated, but otherwise few are.
   if (this.showConsoleOnLaunch) {
     console_checkbox.checked = true;
     console_container.className = '';
@@ -866,7 +866,7 @@ ToneMotion.prototype.getCuesFromServer = function() {
   })
   .catch(error => this.publicError(error));
 
-  this.cueFetchTimeout = setTimeout(this.getCuesFromServer.bind(this), 500);
+  this.cueFetchTimeout = setTimeout(this.getCuesFromServer.bind(this), this.cuePollingInterval);
 };
 
 // Called when server has new cue
